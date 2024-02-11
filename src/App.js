2a78518b5a4d94bc854300c96342ac094f8caf53
gargaman17 <amangarg238@gmail.com>
2024-02-11 20:33:14 +0530

    project initiallized

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ const App = ()=>{
   return(
     <div>
 
-      <h1>Hello World!</h1>
+      <h1 className="text-green-500 text-5xl ">Hello World!</h1>
+      <h2 className="text-4xl">Hello</h2>
     </div>
   )
 }
